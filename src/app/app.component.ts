40ab423487dfb09d928825b535ceaf4c028723a3
Gabriel Toledo <gabrielreb7@gmail.com>
2022-05-07 14:26:38 -0300

    Projeto fatorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'fatorial';
+  numeroDigitado:any = 0;
+  numbers: any[] = [];
+  resultado:any[] = [];
+  resultadoMulti = 1;
+
+  calcular(){
+
+    for (let i = this.numbers.length; i > 0; i--) {
+      this.numbers.pop();
+    }
+    this.resultadoMulti = 1;
+
+    this.numeroDigitado = (<HTMLSelectElement>document.getElementById('numero')).value;
+
+    if(this.numbers.length == 0){
+      for(var i=0; i<this.numeroDigitado;i++){
+        var x = 0;
+       x =  this.numeroDigitado - i
+       if(x != 0){
+        this.numbers.push(x)
+       }
+      }
+    }
+     for(var i = 0; i < this.numbers.length; i++){
+      this.resultadoMulti = this.numbers[i] * this.resultadoMulti
+    } 
+  }
+  
 }
